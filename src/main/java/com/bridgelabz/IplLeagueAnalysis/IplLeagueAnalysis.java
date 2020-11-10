16c5ad018b7e2fb77258b71330448de10dfdca3f
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-11-10 17:19:27 +0530

    STARTED:IPL League Analysis

---
+++ src/main/java/com/bridgelabz/IplLeagueAnalysis/IplLeagueAnalysis.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.IplLeagueAnalysis;
+
+public class IplLeagueAnalysis {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Ipl League analysis");
+    }
+}
